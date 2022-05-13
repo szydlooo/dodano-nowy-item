b2ebd9f9a9a7cc8944d12fc234c9b321452d76a4
oem <oem@DESKTOP-P9U1V2P>
2022-05-13 13:56:03 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using LolClasses;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - rubycrystal
+RubyCrystal rb = new RubyCrystal();
+//sprawdz ile ma hp garen
+g.showHP();
+//kup mu item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+//kup mu drugi item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+Bami B = new Bami();
+g.buyItem(B);
+g.showHP();
+
+
+
+
+
